8fda8bc76f2d9de6b5385cdd790fcde8ae9e64a9
yuliespi <yaespitia86@misena.edu.co>
2022-11-15 10:04:10 -0500

    React-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+const  App = ()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const bootcamps = [
+        {
+            id:1,
+            name: 'PHP Bootcamp',
+            average_rating:8
+        },
+        {
+            id:2,
+            name: 'Javascript Bootcamp',
+            average_rating:10
+        },
+    ]
+
+    return (
+
+        <div>
+            <h1>Este es el componente app</h1>
+            <ul>
+                {bootcamps.map((bootcamp, index)=>(
+                    
+                        <li key={index}>
+                            {bootcamp.name}
+                        </li>
+                    
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
